86717a0e9c2414f8f7814304a397f6e57ca2f911
Carlos Ribeiro <mail@carlosribeiro.me>
2016-08-26 10:02:06 -0300

    Avoid load dotenv on production

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+Dotenv::Railtie.load unless Rails.env.production?
 
 module OpensancaCertifier
   class Application < Rails::Application
