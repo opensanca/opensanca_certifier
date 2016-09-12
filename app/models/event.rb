2ab58ac5fc0682ba8bdc30e806739085eebd6b71
Alberto Rocha <betogrun@gmail.com>
2016-09-12 00:27:50 -0300

    Add relationships to Enrollment

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  has_many :enrollments
+  has_many :users, :through => :enrollments
+
   validates :title,      presence: true
   validates :meetup_url, presence: true
   validates_format_of :meetup_url, with: %r{https?:\/\/(www.)?meetup.com\/(.*)\/opensanca\/events\/(.*)\/}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :enrollments
+  has_many :events, :through => :enrollments
+
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
