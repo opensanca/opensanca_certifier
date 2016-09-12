302fce39373b7229d64300b8402d8cfd4b182400
Alberto Rocha <betogrun@gmail.com>
2016-09-12 16:11:10 +0000

    Update Through syntax to new format

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   has_many :enrollments
-  has_many :users, :through => :enrollments
+  has_many :users, through: :enrollments
 
   validates :title,      presence: true
   validates :meetup_url, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :enrollments
-  has_many :events, :through => :enrollments
+  has_many :events, through: :enrollments
 
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
