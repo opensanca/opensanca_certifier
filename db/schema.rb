d3bdc81a3d6c9cb50d355d6a01a91192bbb55f55
André Luis Gomes <s2it_agomes@uolinc.com>
2016-10-28 11:29:55 -0200

    Migration for add new column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912031732) do
+ActiveRecord::Schema.define(version: 20161028112653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160912031732) do
     t.string   "document"
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["provider"], name: "index_users_on_provider", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
